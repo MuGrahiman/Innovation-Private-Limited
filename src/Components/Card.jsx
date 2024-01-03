aa1f27c1afcb76e2c6d907dbaa00e30efc3bd9be
MuGrahiman <mujeebrahiman2000@gmail.com>
2024-01-03 11:10:33 +0530

    refactor:Card.modified product iterating col as card component

---
+++ src/Components/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Card = ({thumbnail,title,brand,category,description,price}) => {
+    return (
+        <div class="col">
+          <div class="card h-100">
+            <img src={thumbnail} class="card-img-top" alt="..." height="200" />
+            <div class="card-body">
+              <div class="card-header bg-transparent text-start p-0 mb-3">
+                <h4>{title}</h4>
+              </div>
+              <h6 class="card-title p-0 m-0">Brand:{brand}</h6>
+              <p class="card-text ">
+                <small class="text-body-secondary">{category}</small>
+              </p>
+              <p class="card-text  text-truncate p-0 m-0">{description}</p>
+              <p class="text-body-secondary p-0 py-2 m-0 ">
+                Price: <b className="text-warning">${price}</b> 
+                </p>
+                <a href="#" class="btn btn-outline-primary">Add To Cart</a>
+
+              {/* <div class="card-footer bg-transparent p-0 m-0">
+                
+              </div> */}
+            </div>
+          </div>
+        </div>
+      );
+}
+
+export default Card
